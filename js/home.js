3c106d1cf9ad6c856cb58009986bc03d23d442f1
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-10-16 11:22:47 +0700

    22-17 format time span
    - add dayjs
    - add fallback image
    - truncate text

--- js/home.js
+++ js/home.js
@@ -1,39 +1,51 @@
 import postApi from './api/postApi'
-import { setTextContent } from './utils'
+import { setTextContent, truncateText } from './utils'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+// to use fromNow func
+dayjs.extend(relativeTime)
 
 function createPostElement(post) {
   if (!post) return
 
-  try {
-    // find and clone template
-    const postTemplate = document.getElementById('postTemplate')
-    if (!postTemplate) return
+  // find and clone template
+  const postTemplate = document.getElementById('postTemplate')
+  if (!postTemplate) return
 
-    const liElement = postTemplate.content.firstElementChild.cloneNode(true)
-    if (!liElement) return
+  const liElement = postTemplate.content.firstElementChild.cloneNode(true)
+  if (!liElement) return
 
-    // update title, description, author, thumbnail
-    // const titleElement = liElement.querySelector('[data-id="title"]')
-    // if (titleElement) titleElement.textContent = post.title
-    setTextContent(liElement, '[data-id="title"]', post.title)
-    setTextContent(liElement, '[data-id="description"]', post.description)
-    setTextContent(liElement, '[data-id="author"]', post.author)
+  // update title, description, author, thumbnail
+  // const titleElement = liElement.querySelector('[data-id="title"]')
+  // if (titleElement) titleElement.textContent = post.title
+  setTextContent(liElement, '[data-id="title"]', post.title)
+  setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100))
+  setTextContent(liElement, '[data-id="author"]', post.author)
 
-    // const descriptionElement = liElement.querySelector('[data-id="description"]')
-    // if (descriptionElement) descriptionElement.textContent = post.description
+  // const descriptionElement = liElement.querySelector('[data-id="description"]')
+  // if (descriptionElement) descriptionElement.textContent = post.description
 
-    // const authorElement = liElement.querySelector('[data-id="author"]')
-    // if (authorElement) authorElement.textContent = post.author
+  // const authorElement = liElement.querySelector('[data-id="author"]')
+  // if (authorElement) authorElement.textContent = post.author
 
-    const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]')
-    if (thumbnailElement) thumbnailElement.src = post.imageUrl
+  // calculate timespan
+  // console.log('timespan', dayjs(post.updatedAt).fromNow())
+  setTextContent(liElement, '[data-id="timeSpan"]', ` - ${dayjs(post.updatedAt).fromNow()}`)
 
-    // attach events
+  const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]')
+  if (thumbnailElement) {
+    thumbnailElement.src = post.imageUrl
 
-    return liElement
-  } catch (error) {
-    console.log('failed to create post item', error)
+    thumbnailElement.addEventListener('error', () => {
+      console.log('load image error --> use default placeholder')
+      thumbnailElement.src = 'https://via.placeholder.com/1368x400?text=thumbnail'
+    })
   }
+
+  // attach events
+
+  return liElement
 }
 
 function renderPostList(postList) {
--- js/utils/common.js
+++ js/utils/common.js
@@ -4,3 +4,9 @@ export function setTextContent(parent, selector, text) {
   const element = parent.querySelector(selector)
   if (element) element.textContent = text
 }
+
+export function truncateText(text, maxLength) {
+  if (text.length <= maxLength) return text
+
+  return `${text.slice(0, maxLength - 1)}…`
+}
