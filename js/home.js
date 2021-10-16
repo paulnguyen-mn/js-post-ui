7a8127adbe33d4d9576dd12dd10a86b8e38a4451
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-10-16 17:00:16 +0700

    22-19 pagination p1
    - init URL
    - bind click event for links

--- js/home.js
+++ js/home.js
@@ -60,12 +60,60 @@ function renderPostList(postList) {
   })
 }
 
+function handleFilterChange(filterName, filterValue) {
+  // update query params
+  const url = new URL(window.location)
+  url.searchParams.set(filterName, filterValue)
+  history.pushState({}, '', url)
+
+  // fetch API
+  // re-render post list
+}
+
+function handlePrevClick(e) {
+  e.preventDefault()
+  console.log('prev click')
+}
+
+function handleNextClick(e) {
+  e.preventDefault()
+  console.log('next click')
+}
+
+function initPagination() {
+  // bind click event for prev/next link
+  const ulPagination = document.getElementById('pagination')
+  if (!ulPagination) return
+
+  // add click event for prev link
+  const prevLink = ulPagination.firstElementChild?.firstElementChild
+  if (prevLink) {
+    prevLink.addEventListener('click', handlePrevClick)
+  }
+
+  // add click event for next link
+  const nextLink = ulPagination.lastElementChild?.lastElementChild
+  if (nextLink) {
+    nextLink.addEventListener('click', handleNextClick)
+  }
+}
+
+function initURL() {
+  const url = new URL(window.location)
+
+  // update search params if needed
+  if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1)
+  if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6)
+
+  history.pushState({}, '', url)
+}
+
 ;(async () => {
   try {
-    const queryParams = {
-      _page: 1,
-      _limit: 6,
-    }
+    initPagination()
+    initURL()
+
+    const queryParams = new URLSearchParams(window.location.search)
     const { data, pagination } = await postApi.getAll(queryParams)
     renderPostList(data)
   } catch (error) {
