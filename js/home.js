5a48cce78e71980fadfb87f5740e9dafa7283a50
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-10-23 18:00:52 +0700

    Revert "22-23 refactor to make code connected"
    This reverts commit 9ca98c18cff6864223b5a218d952e15c27c924aa.

--- js/home.js
+++ js/home.js
@@ -143,7 +143,7 @@ function initPagination() {
   }
 }
 
-function getDefaultQueryParams() {
+function initURL() {
   const url = new URL(window.location)
 
   // update search params if needed
@@ -151,15 +151,11 @@ function getDefaultQueryParams() {
   if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6)
 
   history.pushState({}, '', url)
-  return url.searchParams
 }
 
-function initSearchInput(queryParams) {
+function initSearchInput() {
   const searchInput = document.getElementById('searchInput')
   if (!searchInput) return
-
-  // set default value
-  searchInput.value = queryParams.get('title_like')
 
   const debounceSearch = debounce(
     (event) => handleFilterChange('title_like', event.target.value),
@@ -168,16 +164,17 @@ function initSearchInput(queryParams) {
   searchInput.addEventListener('input', debounceSearch)
 }
 
-// filter flow
-// filter change --> do something
 ;(async () => {
   try {
-    const queryParams = getDefaultQueryParams()
-
     // attach click event for links
-    initPagination(queryParams)
-    initSearchInput(queryParams)
+    initPagination()
+    initSearchInput()
 
+    // set default pagination (_page, _limit) on URL
+    initURL()
+
+    // render post list based URL params
+    const queryParams = new URLSearchParams(window.location.search)
     const { data, pagination } = await postApi.getAll(queryParams)
     renderPostList(data)
     renderPagination(pagination)
