e5af2a9625505da28d918dc30bbe3a60a5eba06c
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-10-23 21:40:21 +0700

    03-02 click on post item to go to detail page

--- js/utils/post.js
+++ js/utils/post.js
@@ -32,6 +32,13 @@ export function createPostElement(post) {
   }
 
   // attach events
+  // go to post detail when click on div.post-item
+  const divElement = liElement.firstElementChild
+  if (divElement) {
+    divElement.addEventListener('click', () => {
+      window.location.assign(`/post-detail.html?id=${post.id}`)
+    })
+  }
 
   return liElement
 }
