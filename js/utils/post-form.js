d5e040bfd3a6b9d265a83bb90879e108ed5f0446
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-10-29 23:38:45 +0700

    24-05 get form values

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -10,10 +10,41 @@ function setFormValues(form, formValues) {
   setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl)
 }
 
+function getFormValues(form) {
+  const formValues = {}
+
+  // S1: query each input and add to values object
+  // ;['title', 'author', 'description', 'imageUrl'].forEach((name) => {
+  //   const field = form.querySelector(`[name="${name}"]`)
+  //   if (field) formValues[name] = field.value
+  // })
+
+  // S2: using form data
+  const data = new FormData(form)
+  for (const [key, value] of data) {
+    formValues[key] = value
+  }
+
+  return formValues
+}
+
 export function initPostForm({ formId, defaultValues, onSubmit }) {
   const form = document.getElementById(formId)
   if (!form) return
 
-  console.log('form', form)
   setFormValues(form, defaultValues)
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    console.log('submit form')
+
+    // get form values
+    const formValues = getFormValues(form)
+    console.log(formValues)
+
+    // validation
+    // if valid trigger submit callback
+    // otherwise, show validation errors
+  })
 }
