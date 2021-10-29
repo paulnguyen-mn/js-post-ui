40b89bf66fbf552f6ce4a2e65416c51c164d30e0
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-10-29 20:03:54 +0700

    24-02 handle edit click
    - event bubbling
    - should we use stopPropagation()
    - check dom contains

--- js/utils/post.js
+++ js/utils/post.js
@@ -35,8 +35,22 @@ export function createPostElement(post) {
   // go to post detail when click on div.post-item
   const divElement = liElement.firstElementChild
   if (divElement) {
-    divElement.addEventListener('click', () => {
+    divElement.addEventListener('click', (event) => {
+      // S2: if event is triggered from menu --> ignore
+      const menu = liElement.querySelector('[data-id="menu"]')
+      if (menu && menu.contains(event.target)) return
+
       window.location.assign(`/post-detail.html?id=${post.id}`)
+    })
+  }
+
+  // add click event for edit button
+  const editButton = liElement.querySelector('[data-id="edit"]')
+  if (editButton) {
+    editButton.addEventListener('click', (e) => {
+      // S1: prevent event bubbling to parent
+      // e.stopPropagation()
+      window.location.assign(`/add-edit-post.html?id=${post.id}`)
     })
   }
 
