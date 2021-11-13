736d364f160cf264d155179891a0af9ca976b3a4
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-11-13 16:06:15 +0700

    24-27 submit form data

--- js/add-edit-post.js
+++ js/add-edit-post.js
@@ -15,21 +15,30 @@ function removeUnusedFields(formValues) {
   // finally remove imageSource
   delete payload.imageSource
 
+  // remove id if it's add mode
+  if (!payload.id) delete payload.id
+
   return payload
 }
 
+function jsonToFormData(jsonObject) {
+  const formData = new FormData()
+
+  for (const key in jsonObject) {
+    formData.set(key, jsonObject[key])
+  }
+
+  return formData
+}
+
 async function handlePostFormSubmit(formValues) {
-  const payload = removeUnusedFields(formValues)
-  console.log('submit from parent', { formValues, payload })
-  return
   try {
-    // check add/edit mode
-    // S1: based on search params (check id)
-    // S2: check id in formValues
-    // call API
+    const payload = removeUnusedFields(formValues)
+    const formData = jsonToFormData(payload)
+
     const savedPost = formValues.id
-      ? await postApi.update(formValues)
-      : await postApi.add(formValues)
+      ? await postApi.updateFormData(formData)
+      : await postApi.addFormData(formData)
 
     // show success message
     toast.success('Save post successfully!')
--- js/api/postApi.js
+++ js/api/postApi.js
@@ -21,8 +21,15 @@ const postApi = {
     return axiosClient.patch(url, data)
   },
 
+  addFormData(data) {
+    const url = '/with-thumbnail/posts'
+    return axiosClient.post(url, data, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
+  },
+
   updateFormData(data) {
-    const url = `/posts/${data.id}`
+    const url = `/with-thumbnail/posts/${data.get('id')}`
     return axiosClient.patch(url, data, {
       headers: { 'Content-Type': 'multipart/form-data' },
     })
